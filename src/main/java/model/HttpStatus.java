842d7656dc0cf241ebc76cac067b6eb65553cbf9
modi.fy <modi.fy@kakaocorp.com>
2022-09-15 17:47:19 +0900

    http 상태코드 추가

---
+++ src/main/java/model/HttpStatus.java
@@ -0,0 +1,24 @@
+package model;
+
+public enum HttpStatus {
+    OK(200, "OK"),
+    MOVED_PERMANENTLY(301, "Moved Permanently"),
+    BAD_REQUEST(400, "Bad Request"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
+
+    private int code;
+    private String message;
+
+    HttpStatus(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
