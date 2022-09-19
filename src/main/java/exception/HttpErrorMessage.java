fc4fc3606751ec58a434500f0737b5b7c6258d69
modi.fy <modi.fy@kakaocorp.com>
2022-09-19 11:33:18 +0900

    HttpErrorMessage에 상태코드 추가

--- src/main/java/exception/HttpErrorMessage.java
+++ src/main/java/exception/HttpErrorMessage.java
@@ -1,13 +1,21 @@
 package exception;
 
+import model.HttpStatus;
+
 public enum HttpErrorMessage {
-    EMPTY_REQUEST("요청 내용이 비어있습니다."),
-    INVALID_REQUEST("지원하지 않는 형식의 요청입니다.");
+    EMPTY_REQUEST("요청 내용이 비어있습니다.", HttpStatus.BAD_REQUEST),
+    INVALID_REQUEST("지원하지 않는 형식의 요청입니다.", HttpStatus.BAD_REQUEST);
 
     private String message;
+    private HttpStatus status;
 
-    HttpErrorMessage(String errorMessage) {
+    HttpErrorMessage(String errorMessage, HttpStatus httpStatus) {
         this.message = errorMessage;
+        this.status = httpStatus;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
     }
 
     public String getMessage() {
