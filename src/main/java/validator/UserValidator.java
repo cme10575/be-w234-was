6b644bce49965af7b5f9f8cdff2a7094dc36f0a5
modi.fy <cme10575@gmail.com>
2022-09-26 18:35:21 +0900

    Pattern 인스턴스 재사용하도록 수정

--- src/main/java/validator/UserValidator.java
+++ src/main/java/validator/UserValidator.java
@@ -7,25 +7,25 @@ import java.util.Map;
 import java.util.regex.Pattern;
 
 public class UserValidator {
-    private static String idReg = "^[a-zA-Z0-9]{4,20}$";
-    private static String passwordReg = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d~!@#$%^&*()+|=]{8,20}$";
-    private static String nameReg = "^[a-zA-Z가-힣]{2,30}$";
-    private static String emailReg = "^[a-z0-9A-Z._-]*@[a-z0-9A-Z]*\\.[a-zA-Z]*$";
+    private static final Pattern ID = Pattern.compile("^[a-zA-Z\\d]{4,20}$");
+    private static final Pattern PASSWORD = Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d~!@#$%^&*()+|=]{8,20}$");
+    private static final Pattern NAME = Pattern.compile("^[a-zA-Z가-힣]{2,30}$");
+    private static final Pattern EMAIL = Pattern.compile("^[a-z0-9A-Z._-]*@[a-z0-9A-Z]*\\.[a-zA-Z]*$");
 
     public static boolean isValidId(String id) {
-        return Pattern.matches(idReg, id);
+        return ID.matcher(id).matches();
     }
 
     public static boolean isValidPassword(String password) {
-        return Pattern.matches(passwordReg, password);
+        return PASSWORD.matcher(password).matches();
     }
 
     public static boolean isValidName(String name) {
-        return Pattern.matches(nameReg, name);
+        return NAME.matcher(name).matches();
     }
 
     public static boolean isValidEmail(String email) {
-        return Pattern.matches(emailReg, email);
+        return EMAIL.matcher(email).matches();
     }
 
     public static boolean validateUser(Map<String, String> params) {
