46e41c614c052c60ab726d4bcaa6d52204a8f3d6
modi.fy <cme10575@gmail.com>
2022-09-29 19:43:21 +0900

    Controller 매핑 방식 변경
    Pattern 객체를 매번 생성하는 .matches() 제거
    분기문이 많아짐에 따라 유지보수를 위해 반복문 형식으로 변경

--- src/main/java/webserver/HandlerMapper.java
+++ src/main/java/webserver/HandlerMapper.java
@@ -8,38 +8,35 @@ import exception.HttpErrorMessage;
 import exception.HttpException;
 import model.HttpRequest;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
 public class HandlerMapper {
     private static UserController userController;
     private static FileController fileController;
     private static MemoController memoController;
 
+    private Map<Pattern, Controller> controllerMap;
+
     public HandlerMapper() {
         userController = new UserController();
         fileController = new FileController();
         memoController = new MemoController();
+        controllerMap = new HashMap<>();
+        controllerMap.put(Pattern.compile("/index.html"), memoController);
+        controllerMap.put(Pattern.compile("(.*).css"), fileController);
+        controllerMap.put(Pattern.compile("(.*).html"), fileController);
+        controllerMap.put(Pattern.compile("/user(.*)"), userController);
+        controllerMap.put(Pattern.compile("/memo(.*)"), memoController);
+        controllerMap.put(Pattern.compile("(.*)"), fileController);
     }
 
     public Controller findHandler(HttpRequest request) {
-        if (request.getPath().matches("/index.html")) {
-            return memoController;
-        } else if (request.getPath().matches("(.*).css")) {
-            return fileController;
-        } else if (request.getPath().matches("(.*).html")) {
-            return fileController;
-        } else if (request.getPath().matches("(.*).js")) {
-            return fileController;
-        } else if (request.getPath().matches("(.*).png")) {
-            return fileController;
-        } else if (request.getPath().matches("(.*).ico")) {
-            return fileController;
-        } else if (request.getPath().matches("(.*).ttf")) {
-            return fileController;
-        } else if (request.getPath().matches("/user(.*)")) {
-            return userController;
-        } else if (request.getPath().matches("/memo(.*)")) {
-            return memoController;
-        } else {
-            throw new HttpException(HttpErrorMessage.INVALID_REQUEST);
+        for (var entry : controllerMap.entrySet()) {
+            if (entry.getKey().matcher(request.getPath()).matches())
+                return entry.getValue();
         }
+        throw new HttpException(HttpErrorMessage.INVALID_REQUEST);
     }
 }
